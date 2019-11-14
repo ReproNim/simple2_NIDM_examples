d17da65ef5b7c01c6105c436541a0e1f0d3409bb
David Keator <dbkeator@uci.edu>
2019-11-14 13:19:18 -0800

    updates to ants helper script

--- add_ants_segvols.py
+++ add_ants_segvols.py
@@ -26,8 +26,11 @@ def main():
                                                                        './datasets.datalad.org/abide/RawDataBIDS/SDSU/sub-0050199'
                                                                        '...')
     parser.add_argument('-new','--new', action='store_true', required=False, help="If flag set then new NIDM files will")
+    parser.add_argument('-forcenidm','--forcenidm', action='store_true', required=False, help="If flag set then data will be"
+                                        "added to existing NIDM file regardless if subject already exists.")
 
     args = parser.parse_args()
+    new  = args.new
 
     with open(args.subj,'r') as f:
         for line in f:
@@ -36,36 +39,48 @@ def main():
             #strip training \n from line
             line=line.rstrip('\n')
             loc = line.find("sub")
-            if not args.new:
+            if not new:
                 if not isfile(join(line[:loc-1],"nidm.ttl")):
                     print("No existing NIDM-E file for site: %s" %line[:loc-1])
                     print("Skipping subject: %s" %line[loc:])
                     continue
-
+            else:
+                #check if nidm.ttl file actually exists and if so use it otherwise create from scratch
+                 if isfile(join(line[:loc-1],"nidm.ttl")):
+                     new = False
             # set up command to add brain volumes from Amazon bucket
             # -f "https://fcp-indi.s3.amazonaws.com/data/Projects/ABIDE/Outputs/mindboggle_swf/mindboggle/ants_subjects/sub-0050002/antslabelstats.csv,
             # https://fcp-indi.s3.amazonaws.com/data/Projects/ABIDE/Outputs/mindboggle_swf/mindboggle/ants_subjects/sub-0050002/antsbrainvols.csv,
             # https://fcp-indi.s3.amazonaws.com/data/Projects/ABIDE/Outputs/mindboggle_swf/mindboggle/ants_subjects/sub-0050002/antsBrainSegmentation.nii.gz"
 
             base_url = "https://fcp-indi.s3.amazonaws.com/data/Projects/"
+            #get dataset (abide or adhd200) from line
             if "abide" in line:
-                base_url = base_url + "ABIDE/"
+                cmd=cmd + "ABIDE/"
             elif "adhd200" in line:
-                base_url = base_url + "ADHD200/"
+                cmd=cmd + "ADHD200/"
             elif "corr" in line:
-
+                cmd=cmd + "CORR/"
             else:
                 print("Error, can't find dataset (abide | adhd200) in line: %s" %line)
                 print("Skipping...")
                 continue
+
             base_url = base_url + "Outputs/mindboggle_swf/mindboggle/ants_subjects/"
 
             cmd="antsegstats2nidm -f \"" + base_url + line[loc:] + "/antslabelstats.csv," + \
                     base_url + line[loc:] + "/antsbrainvols.csv," + \
                     base_url + line[loc:] + "/antsBrainSegmentation.nii.gz\" -subjid " + line[loc+4:] 
 
-            cmd = cmd + " -n " + line[:loc] + "nidm.ttl -o " + line[:loc] + \
+            if not new:
+                cmd = cmd + " -n " + line[:loc] + "nidm.ttl -o " + line[:loc] + \
                     "nidm.ttl"
+                if args.forcenidm is not False:
+                    cmd = cmd + " -forcenidm "
+            else:
+                cmd = cmd + " -o " + join(line[:loc],"nidm.ttl")
+                if args.forcenidm is not False:
+                    cmd = cmd + " -forcenidm "
 
             # execute command
             print(cmd)
