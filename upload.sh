fb993425e3949faae7621fe1336e7a944583713b
Yaroslav Halchenko <debian@onerussian.com>
2024-02-09 14:46:44 -0500

    upload -- provide path directly without cp, but also try up to 5 times upon exit 56
    if blazegraph is still starting we might fail to upload for a bit -- here we will retry a number
    of times

--- upload.sh
+++ upload.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-set -eu
+set -u
 
-if test -f "test.ttl"; then
-    rm test.ttl
-fi
-cp $1 test.ttl
-echo "uploading: $1"
-curl -X POST -H 'Content-Type: text/turtle' --data-binary '@test.ttl'  http://localhost:8889/bigdata/sparql
+for attempt in {1..5}; do
+    echo "Uploading #$attempt: $1"
+    curl -X POST -H 'Content-Type: text/turtle' --data-binary "@$1"  http://localhost:8889/bigdata/sparql
+    ex="$?"
+    case "$ex" in
+        0) break;;
+        56) echo "Seems not ready yet, will retry after a sleep";
+            sleep 2;;
+        *) exit $ex
+    esac
+done
+
 echo "Finished uploading"
