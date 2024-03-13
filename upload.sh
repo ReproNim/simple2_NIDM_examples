868f6b1e1f531f7e5230bbd23848d831aee3b0ec
Yaroslav Halchenko <debian@onerussian.com>
2024-03-13 18:08:56 -0400

    Use variable for API URL in upload.sh

--- upload.sh
+++ upload.sh
@@ -3,13 +3,13 @@ set -u
 
 for attempt in {1..5}; do
     echo "Uploading #$attempt: $1"
-    curl --silent -X POST -H 'Content-Type: text/turtle' --data-binary "@$1"  http://localhost:8889/bigdata/sparql
+    curl --silent -X POST -H 'Content-Type: text/turtle' --data-binary "@$1"  ${GRAPHDB_API_URL:-http://localhost:8889/bigdata/sparql}
     ex="$?"
     case "$ex" in
         0) break;;
         56) echo "Seems not ready yet, will retry after a sleep";
             sleep 2;;
-        *) exit $ex
+        *) echo "Exiting with $ex"; exit $ex;;
     esac
 done
 
